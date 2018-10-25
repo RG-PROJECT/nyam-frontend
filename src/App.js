5a4bfda5d4b73dcc835c993cde5be683db4fae5e
godori <gej.okpo@gmail.com>
2018-10-26 01:04:12 +0900

    fetch restAPI data

--- src/App.js
+++ src/App.js
@@ -1,105 +1,49 @@
 import React, { Component } from "react";
+import "./App.css";
 import Card from "./components/card";
 import WriteForm from "./components/writeForm";
 import { connect } from "react-redux";
 import ReduxTest from "./components/reduxTest";
 
-const commentArr = [
-  {
-    id: 1,
-    status: "OK",
-    regdate: "20181011",
-    author: "TOTO",
-    text: "good job godori 👏"
-  },
-  {
-    id: 19,
-    status: "OK",
-    regdate: "20181012",
-    author: "KONG",
-    text: "ㅇㅇ"
-  }
-];
-
-const cardInfo1 = {
-  id: 1,
-  status: "OK",
-  title: "my meal~~",
-  author: "godori",
-  regdate: "20181010", // Date Type 시간추가 (시간 대충가는 라이브러리)
-  contents: {
-    img: "https://picsum.photos/200",
-    text: "나는 이런 밥을 먹었고 점심은 이러했다.\n저녁은 이거 먹을거임"
-  },
-  reaction: {
-    comment: commentArr
-  },
-  comments: commentArr
-};
-
-const cardInfo2 = {
-  id: 2,
-  status: "OK",
-  title: "밥뭇나",
-  author: "TOTO",
-  regdate: "20181016",
-  contents: {
-    img: "https://picsum.photos/201",
-    text:
-      "토토는 토토해 토토는 토토해 토토는 토토해 토토는 토토해 토토는 토토해 토토는 토토해 토토는 토토해 토토는 토토해 "
-  },
-  reaction: {
-    comment: commentArr
-  }
-};
-
-const cardInfo3 = {
-  id: 3,
-  status: "OK",
-  title: "더미데이터 귀찮아",
-  author: "Deer.White",
-  regdate: "20181011",
-  contents: {
-    img: "https://picsum.photos/203",
-    text: "냥냥구들 귀여워 ~! "
-  },
-  reaction: {
-    comment: commentArr
-  }
-};
-
 class App extends Component {
-  state = {
-    cardList: [cardInfo1, cardInfo2],
-    title: "",
-    content: ""
-  };
+  state = {};
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        cardList: [cardInfo3, ...this.state.cardList]
-      });
-    }, 2000);
+    this._getCards();
   }
 
+  _getCards = async () => {
+    const cards = await this._callApi();
+    this.setState({
+      cards
+    });
+  };
+
+  _callApi = () => {
+    return fetch("http://localhost:3000/cards")
+      .then(result => result.json())
+      .catch(err => console.log(err));
+  };
+
+  _renderCards = () => {
+    const cards = this.state.cards.map(card => {
+      return <Card key={card._id} data={card} />;
+    });
+    return cards;
+  };
+
+  _renderLoading = () => {
+    return <h1>Loading...</h1>;
+  };
+
   render() {
+    const { cards } = this.state;
+    {
+      console.log(cards);
+    }
     return (
-      <div className="App">
-        <ReduxTest />
-        <WriteForm />
-        {this.state.cardList.map(card => {
-          return (
-            <Card
-              title={card.title}
-              author={card.author}
-              regdate={card.regdate}
-              contents={card.contents}
-              reaction={card.reaction}
-              key={card.id}
-            />
-          );
-        })}
+      <div className={cards ? "App" : "Loading"}>
+        {this.state.cards ? this._renderCards() : this._renderLoading()}
       </div>
     );
   }
--- src/components/card.js
+++ src/components/card.js
@@ -1,38 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import PropTypes from 'prop-types';
-import Contents from './contents';
-import Reaction from './reaction';
-import './card.css';
+import Contents from "./contents";
+import Reaction from "./reaction";
+import "./card.css";
 
 class Card extends Component {
-    render(){
-        return (
-            <div className="card">
-                <Header
-                    title={this.props.title}
-                    author={this.props.author}
-                    regdate={this.props.regdate}
-                />
-                <Contents
-                    img={this.props.contents.img}
-                    text={this.props.contents.text}
-                />
-                <Reaction reaction={this.props.reaction} />
-            </div>
-        )
-    }
+  render() {
+    const card = this.props.data;
+    return (
+      <div className="card">
+        <Header
+          title={card.title}
+          author={card.author}
+          regdate={card.regdate}
+        />
+        <Contents img={card.img} text={card.text} />
+        {/* <Reaction reaction={card.reaction} /> */}
+      </div>
+    );
+  }
 }
 
 class Header extends Component {
-    render() {
-        return (
-            <div className="cardHeader">
-                <h1>{this.props.title}</h1>
-                <div className="cardAuthor">{this.props.author}</div>
-                <div className="cardRegdate">{this.props.regdate}</div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="cardHeader">
+        <h3>{this.props.title}</h3>
+        <div className="cardAuthor">{this.props.author}</div>
+        <div className="cardRegdate">{this.props.regdate}</div>
+      </div>
+    );
+  }
 }
 
 export default Card;
