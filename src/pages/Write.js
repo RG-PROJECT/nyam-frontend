2284138ea3e8bd17c36747f878bc83e8270dff16
godori <gej.okpo@gmail.com>
2019-01-26 04:41:40 +0900

    add write page

---
+++ src/pages/Write.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./write.css";
+
+const Write = () => {
+  return (
+    <React.Fragment>
+      <div className="pageWrapper">
+        <h1>Create Card Page</h1>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Write;
